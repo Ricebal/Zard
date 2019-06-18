fb04b38d91c171a05f1802d7e4ac3ba5614cf1e4
Nicander Mohrmann <s1103064@student.windesheim.nl>
2019-06-19 01:43:52 +0200

    initial commit

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+    [SerializeField] bool m_airControl = false;
+    [SerializeField] float m_jumpForce = 400f;
+    private bool m_facingRight = true;
+    private Rigidbody2D m_rigidbody2D;
+    private Vector3 m_velocity = Vector3.zero;
+    private bool m_grounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Move(float move, bool jump)
+    {
+        if (m_airControl || m_grounded)
+        {
+            Vector3 targetVelocity = new Vector2(move * 10f, m_rigidbody2D.velocity.y);
+            m_rigidbody2D.velocity = targetVelocity;
+            if ((move > 0 && !m_facingRight) || (move < 0 && m_facingRight))
+            {
+                Flip();
+            }
+        }
+
+        if (m_grounded && jump)
+        {
+            // m_grounded = false;
+            m_rigidbody2D.AddForce(new Vector2(0f, m_jumpForce));
+        }
+    }
+
+    void Flip()
+    {
+        m_facingRight = !m_facingRight;
+
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Animator m_animator;
+    [SerializeField] private CharacterController2D m_controller = null;
+    [SerializeField] private float m_speed = 40f;
+
+    private float m_horizontalMove = 0f;
+    private bool m_jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_horizontalMove = Input.GetAxisRaw("Horizontal") * m_speed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            m_jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        m_controller.Move(m_horizontalMove * Time.fixedDeltaTime, m_jump);
+        m_jump = false;
+    }
+}
